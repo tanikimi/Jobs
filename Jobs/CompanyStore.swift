5f575ff7817e4e9d5b8469f8e1772dca2062ea2b
tanikimi <tanikimi.dev@icloud.com>
2026-03-08 15:06:01 +0900

    :tada: データ保存機能の実装

--- Jobs/CompanyStore.swift
+++ Jobs/CompanyStore.swift
@@ -3,7 +3,15 @@ import Observation
 
 @Observable
 class CompanyStore {
-    var companies: [Company] = []
+    var companies: [Company] = [] {
+        didSet{ save() }
+    }
+    
+    private let saveURL = URL.documentsDirectory.appending(path: "companies.json")
+    
+    init() {
+        load()
+    }
 
     func add(_ company: Company) {
         companies.append(company)
@@ -16,6 +24,24 @@ class CompanyStore {
     func update(_ company: Company) {
         if let index = companies.firstIndex(where: { $0.id == company.id }) {
             companies[index] = company
+        }
+    }
+    
+    private func save() {
+        do {
+            let data = try JSONEncoder().encode(companies)
+            try data.write(to: saveURL)
+        } catch {
+            print("保存失敗: \(error)")
+        }
+    }
+    
+    private func load() {
+        do {
+            let data = try Data(contentsOf: saveURL)
+            companies = try JSONDecoder().decode([Company].self, from: data)
+        } catch {
+            companies = []
         }
     }
 }
